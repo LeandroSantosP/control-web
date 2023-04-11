422ce31159174a96cf5f87b0d3898c238a6250c8
LeandroSantosP <skp.tridimensional@gmail.com>
2023-04-10 22:48:43 -0300

    feat:pedding integrate with ghaps lib

--- src/components/Molecules/TransactionGraphs/TransactionGraphs.tsx
+++ src/components/Molecules/TransactionGraphs/TransactionGraphs.tsx
@@ -1,138 +1,252 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
-import { useTransactionContext } from '../../../shared/contexts';
+import { ArrowsCounterClockwise } from '@phosphor-icons/react';
+import { Transaction, useTransactionContext } from '../../../shared/contexts';
 import * as S from './TransactionGraphsStyles';
+import { toMoney } from 'vanilla-masker';
 
 export const TransactionGraphs = () => {
+   const [currentTransactionType, setCurrentTransactionType] = useState<
+      'revenue' | 'expense'
+   >('revenue');
    const { transaction } = useTransactionContext();
+   const [muOptions, setMuOptions] = useState<
+      ApexCharts.ApexOptions | undefined
+   >(undefined);
 
-   const expenseTransactions = transaction?.transactions
-      .filter((item) => {
-         const currentYear = new Date(item.due_date as string).getFullYear();
-         return item.type === 'expense' && currentYear === 2023;
-      })
-      .sort((a, b) => {
-         const monthA_expense = new Date(a.due_date as string).getMonth();
-         const monthB_expense = new Date(b.due_date as string).getMonth();
+   const GetTransactionType = useCallback(
+      (type: 'expense' | 'revenue') => {
+         const TransactionType = transaction?.transactions
+            .filter((item) => {
+               const getCorrespondeDateType =
+                  type === 'expense' ? item.due_date : item.filingDate;
+               const currentYear = new Date(
+                  getCorrespondeDateType as string
+               ).getFullYear();
 
-         return monthA_expense - monthB_expense;
-      });
+               return item.type === type && currentYear === 2023;
+            })
+            .sort((a, b) => {
+               let typeChoses = {} as any;
 
-   const groupedByMonth = expenseTransactions?.reduce((storage, curr) => {
-      const month = curr.due_date?.substr(0, 7) as string;
+               if (type === 'expense') {
+                  typeChoses = {
+                     a: a.due_date,
+                     b: b.due_date,
+                  };
+               } else if (type == 'revenue') {
+                  typeChoses = {
+                     a: a.filingDate,
+                     b: b.filingDate,
+                  };
+               }
 
-      if (!storage[month]) {
-         storage[month] = [];
+               const monthA = new Date(typeChoses.a as string).getMonth();
+               const monthB = new Date(typeChoses.b as string).getMonth();
+               return monthA - monthB;
+            });
+
+         return TransactionType;
+      },
+      [transaction?.transactions]
+   );
+
+   const GroupedByMonth = useCallback(
+      (
+         transactions: Transaction[] | undefined,
+         type: 'expense' | 'revenue'
+      ) => {
+         const groupByMount = transactions?.reduce((storage, crr) => {
+            const getCorrespondeDateType =
+               type === 'expense' ? crr.due_date : crr.filingDate;
+
+            const month = getCorrespondeDateType?.substr(0, 7) as string;
+
+            if (!storage[month]) {
+               storage[month] = [];
+            }
+            storage[month].push(crr);
+
+            return storage;
+         }, {} as { [key: string]: any });
+
+         return groupByMount;
+      },
+      []
+   );
+
+   useEffect(() => {
+      const TransactionsResult = GetTransactionType(currentTransactionType);
+
+      const groupedByMonthExpense = GroupedByMonth(
+         TransactionsResult,
+         currentTransactionType
+      );
+
+      const result = {} as { [key: string]: any };
+
+      for (const month in groupedByMonthExpense) {
+         const group = groupedByMonthExpense[month];
+
+         result[month] = {
+            max: -Infinity,
+            min: Infinity,
+         };
+
+         for (const item of group) {
+            const value = parseFloat(item.value);
+            if (isNaN(value)) continue;
+            result[month].max = Math.max(result[month].max, value);
+            result[month].min = Math.min(result[month].min, value);
+         }
       }
-      storage[month].push(curr);
 
-      return storage;
-   }, {} as { [key: string]: any[] });
+      const data = Object.keys(result).map((month) => ({
+         month,
+         minimalCurrentValue: result[month].max,
+         majorCurrentValue: result[month].min,
+      }));
 
-   const result = {} as { [key: string]: any };
+      const Months = [
+         {
+            name: 'Jan',
+            ref: '01',
+         },
+         {
+            name: 'Fev',
+            ref: '02',
+         },
+         {
+            name: 'mar',
+            ref: '03',
+         },
+         {
+            name: 'abr',
+            ref: '04',
+         },
+         {
+            name: 'mai',
+            ref: '05',
+         },
+         {
+            name: 'jun',
+            ref: '06',
+         },
+         {
+            name: 'jul',
+            ref: '07',
+         },
+         {
+            name: 'ago',
+            ref: '08',
+         },
+         {
+            name: 'set',
+            ref: '09',
+         },
+         {
+            name: 'out',
+            ref: '10',
+         },
+         {
+            name: 'nov',
+            ref: '11',
+         },
+         {
+            name: 'dez',
+            ref: '12',
+         },
+      ];
 
-   for (const month in groupedByMonth) {
-      const group = groupedByMonth[month];
-
-      result[month] = {
-         max: -Infinity,
-         min: Infinity,
-      };
-
-      for (const item of group) {
-         const value = parseFloat(item.value);
-         if (isNaN(value)) continue;
-         result[month].max = Math.max(result[month].max, value);
-         result[month].min = Math.min(result[month].min, value);
-      }
-   }
-
-   const data = Object.keys(result).map((month) => ({
-      month,
-      minimalExpense: result[month].max,
-      majorExpense: result[month].min,
-   }));
-
-   // const test = expenseTransactions?.reduce(
-   //    (storage, crr) => {
-   //       const value = parseFloat(crr.value);
-
-   //       if (!isNaN(value) && value > storage.highestExpense.value) {
-   //          storage.highestExpense = crr as any;
-   //       }
-
-   //       if (!isNaN(value) && value < storage.minorExpense.value) {
-   //          storage.minorExpense = crr as any;
-   //       }
-
-   //       return storage;
-   //    },
-   //    {
-   //       highestExpense: { value: -Infinity },
-   //       minorExpense: { value: Infinity },
-   //    }
-   // );
-
-   const [muOptions, setMuOptions] = useState({
-      options: {
-         chart: {
-            id: 'basic-bar',
+      setMuOptions({
+         legend: {
+            position: 'top',
+         },
+         tooltip: {
+            enabled: true,
+            fillSeriesColor: true,
+            theme: 'dark',
          },
          plotOptions: {
             bar: {
-               borderRadius: 3,
-               colors: {
-                  ranges: [
-                     {
-                        from: 1,
-                        to: 3,
-                        color: 'red',
+               borderRadiusApplication: 'around',
+               borderRadiusWhenStacked: 'last',
+               dataLabels: {
+                  position: 'top',
+                  orientation: 'horizontal',
+                  total: {
+                     enabled: true,
+                     style: {
+                        fontSize: '1px',
                      },
-                  ],
-                  backgroundBarColors: [],
-                  backgroundBarOpacity: 1,
-                  backgroundBarRadius: 0,
+                  },
                },
+               columnWidth: '40px',
             },
          },
          xaxis: {
-            categories: [
-               'Jan',
-               'Fev',
-               'mar',
-               'abr',
-               'mai',
-               'jun',
-               'jul',
-               'ago',
-               'set',
-               'out',
-               'nov',
-               'dez',
-            ],
+            categories: data.map((item) => {
+               const formattedMonth = item.month.slice(-2);
+
+               const response = Months.map((item) => {
+                  if (item.ref.includes(formattedMonth)) {
+                     return item.name;
+                  }
+                  return;
+               }).filter((i) => i !== undefined);
+               return response;
+            }),
+            labels: {
+               formatter: function (value) {
+                  return value;
+               },
+            },
+            type: 'category',
          },
 
          series: [
             {
-               name: 'Maiores Despesas',
-               data: [23, 40, 30, 2, 23, 23, 88, 130],
+               name: `Menores Despesas (Por MES)`,
+               color: 'rgba(176, 159, 80, 0.66)',
+               data: data.map((item) => {
+                  return item.majorCurrentValue;
+               }),
             },
             {
-               name: 'Menores Despesas',
-               data: [4, 2, 230],
+               name: 'Maiores Despesas (Por MES)',
+
+               color: 'rgba(80, 176, 149, 0.66)',
+               data: data?.map((item) => {
+                  return item.minimalCurrentValue;
+               }),
             },
          ],
-      },
-   });
+      });
+   }, [currentTransactionType, GroupedByMonth, GetTransactionType]);
 
    return (
       <S.WrapperMain>
-         <Chart
-            options={muOptions.options}
-            series={muOptions.options.series}
-            type="bar"
-            width={500}
-         />
+         <S.ToggleButton
+            onClick={() =>
+               setCurrentTransactionType((prev) => {
+                  if (prev === 'expense') {
+                     return 'revenue';
+                  }
+                  return 'expense';
+               })
+            }
+         >
+            <ArrowsCounterClockwise size={20} />
+            {currentTransactionType === 'expense' ? 'Receitas' : 'Dispensas'}
+         </S.ToggleButton>
+         {muOptions && (
+            <Chart
+               options={muOptions}
+               series={muOptions?.series}
+               width={500}
+               type="bar"
+            />
+         )}
       </S.WrapperMain>
    );
 };
--- src/components/Molecules/TransactionGraphs/TransactionGraphsStyles.ts
+++ src/components/Molecules/TransactionGraphs/TransactionGraphsStyles.ts
@@ -3,4 +3,22 @@ import styled from 'styled-components';
 export const WrapperMain = styled('section')`
    display: flex;
    height: 100%;
+   position: relative;
+`;
+
+export const ToggleButton = styled('button')`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   position: absolute;
+   background-color: ${(props) => props.theme.colors.Dark};
+   border: 1px solid ${(props) => props.theme.colors.TimberWhite};
+   padding: 0.2rem;
+   top: 0;
+   border-radius: 0.3rem;
+   z-index: 1;
+   cursor: pointer;
+   &:hover {
+      filter: brightness(1.9);
+   }
 `;
